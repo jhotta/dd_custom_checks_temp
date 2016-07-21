4c31549ffd7c009a78b9028706ee476abe18ac28
Naotaka Jay HOTTA <fukuzo@cybertron.co.jp>
2016-07-21 17:12:54 +0900

    added tags

--- checks.d/customcheck_sample.py
+++ checks.d/customcheck_sample.py
@@ -9,9 +9,10 @@ class CustomCheck(AgentCheck):
         ntp_stats = ntplib.NTPClient().request(**req_args)
         ntp_ts = ntp_stats.recv_time
         service_check_msg = 'this is test message for custom check'
+        tags = ['host:1604TLSi']
 
         status = AgentCheck.OK
 
-        self.service_check('customcheck.myapp', status, timestamp=ntp_ts, message=service_check_msg)
+        self.service_check('customcheck.myapp', status=status, timestamp=ntp_ts, message=service_check_msg, tags=tags)
         self.gauge('customcheck.myapp', 1)
 
