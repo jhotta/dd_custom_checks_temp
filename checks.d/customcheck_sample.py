53e93607a449880955feb53b4e51ac3c0f65397b
Naotaka Jay HOTTA <fukuzo@cybertron.co.jp>
2016-07-21 16:52:13 +0900

    added check status

--- checks.d/customcheck_sample.py
+++ checks.d/customcheck_sample.py
@@ -1,5 +1,15 @@
+import ntplib
 from checks import AgentCheck
 
-class HelloCheck(AgentCheck):
+class CustomCheck(AgentCheck):
     def check(self, instance):
+
+        ntp_stats = ntplib.NTPClient().request(**req_args)
+        ntp_ts = ntp_stats.recv_time
+        service_check_msg = 'this is test message for custom check'
+
+        status = AgentCheck.OK
+
+        self.service_check('customcheck.myapp', status, timestamp=ntp_ts, message=service_check_msg)
         self.gauge('customcheck.myapp', 1)
+
